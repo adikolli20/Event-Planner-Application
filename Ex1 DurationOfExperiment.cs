a7e2d0a5bee4e73101747e4fcde841dffb5c7bc7
adikolli20 <82210633+adikolli20@users.noreply.github.com>
2022-05-02 01:38:16 -1000

    Add files via upload

---
+++ Ex1 DurationOfExperiment.cs
@@ -0,0 +1,31 @@
+﻿
+using  System;
+namespace Ex1_Duration_of_experiment
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the total nr in seconds");
+            int total = Convert.ToInt32(Console.ReadLine());
+            int seconds = total % 60;
+            int hours = total / 3600;
+            int minutes = (total - hours * 3600) / 60;
+            string output = "The duration of experiment was : ";
+            if (hours!=0)
+            {
+                output += hours + " hours ";
+
+            }
+            if (minutes!=0)
+            {
+                output += minutes + " minutes ";
+            }
+            if (seconds!=0)
+            {
+                output += seconds + " seconds";
+            }
+            Console.Out.WriteLine(output);
+        }
+    }
+}
